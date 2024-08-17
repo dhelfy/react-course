8aa5278bbcd946c1e1ba208320f7365d58e1a1c0
dhelfy <dhcode4business@gmail.com>
2024-08-17 20:18:23 +0500

    added react-transition-group and animation for posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,21 @@
+import { CSSTransition, TransitionGroup } from "react-transition-group"
 import Post from "./Post"
 
 export default function PostsList({ posts, deletePost, ...props}) {
-    let posts_list = posts.map(function (post) { return (<Post post_info={post} key={post.id} deletePost={deletePost}/>) })
+    let posts_list = posts.map(function (post) { 
+        return (
+            <CSSTransition key={post.id} classNames="item" timeout={500}>
+                <Post post_info={post} deletePost={deletePost}/>
+            </CSSTransition>
+        ) 
+    })
     if (!posts.length) {
         return <h1>There is not a single post yet</h1>
     }
 
     return (
-        posts_list
+        <TransitionGroup>
+            {posts_list}
+        </TransitionGroup>
     )
 }
